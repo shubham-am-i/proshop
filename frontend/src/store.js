7fd157431e0cc2411420524a37cf255b741dc80f
shubham-am-i <patilshubham2k21@gmail.com>
2022-07-11 15:09:08 +0530

    Order Details Reducer & Action + Order Screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -9,7 +9,7 @@ import {
   userRegisterReducer,
   userUpdateProfileReducer,
 } from './reducers/userReducers'
-import { orderCreateReducer } from './reducers/orderReducers'
+import { orderCreateReducer, ordereDetailsReducer } from './reducers/orderReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -20,6 +20,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
+  orderDetails: ordereDetailsReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
