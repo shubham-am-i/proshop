a8bc8b63bbc942b7667d189dff0dc3a5a3e7a637
shubham-am-i <patilshubham2k21@gmail.com>
2022-07-21 13:28:36 +0530

    add port to .env

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import dotenv from 'dotenv'
 import path from 'path'
 import colors from 'colors'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
@@ -7,9 +8,9 @@ import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
-const PORT = 5000
 
-
+dotenv.config()
+const PORT = process.env.PORT || 5000
 
 connectDB()
 
@@ -28,20 +29,15 @@ app.use('/api/upload', uploadRoutes)
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  )
-
+app.get('*', (req, res) =>
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+)
 
 app.use(notFound)
 app.use(errorHandler)
 
-app.listen(
-  PORT,
-  console.log(
-    `Server running on port ${PORT}`.yellow.bold
-  )
-)
+app.listen(PORT, '0.0.0.0', function () {
+  console.log('Listening on Port 5000')
+})
