a8e7bae2663d1b32088f9539acb60cfbbe979051
shubham-am-i <patilshubham2k21@gmail.com>
2022-07-21 13:38:30 +0530

    change secret from auth middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -8,7 +8,7 @@ const protect = asyncHandler(async (req, res, next) => {
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     try {
       token = req.headers.authorization.split(' ')[1]
-      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      const decoded = jwt.verify(token, 'abc123')
       //   console.log(decoded)
       req.user = await User.findById(decoded.id).select('-password')
       next()
@@ -26,11 +26,11 @@ const protect = asyncHandler(async (req, res, next) => {
 })
 
 const isAdmin = (req, res, next) => {
-  if(req.user && req.user.isAdmin) {
+  if (req.user && req.user.isAdmin) {
     next()
-  }else {
+  } else {
     res.status(401)
-    throw new Error("Not authorized as an admin")
+    throw new Error('Not authorized as an admin')
   }
 }
 
