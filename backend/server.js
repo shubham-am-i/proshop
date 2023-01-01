f2153e436f02b838b0e5867802f0b69927d2ce33
shubham-am-i <patilshubham2k21@gmail.com>
2023-01-01 11:09:45 +0530

    remove async bootstrap method

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 // native imports
 import path from 'path'
-import { fileURLToPath } from 'url'
+import {fileURLToPath} from 'url'
 // external package imports
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import morgan from 'morgan'
 // local imports
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 // import routers
 import productRoutes from './routes/productRoutes.js'
@@ -16,16 +16,20 @@ import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
+connectDB()
 const app = express()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({extended: true}))
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
-app.use('/uploads', express.static(path.resolve(__dirname, '/uploads')))
+app.use(
+  '/uploads',
+  express.static(path.resolve(__dirname, '/uploads'))
+)
 app.use(express.static(path.resolve(__dirname, '../frontend/build')))
 
 // mount routers
@@ -37,7 +41,9 @@ app.use('/api/upload', uploadRoutes)
 // app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
 app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'))
+  res.sendFile(
+    path.resolve(__dirname, '../frontend', 'build', 'index.html')
+  )
 )
 
 app.use(notFound)
@@ -45,11 +51,6 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-const bootstrap = async () => {
-  await connectDB()
-  app.listen(PORT, () =>
-    console.log(`Server is running on port ${PORT}`.yellow.bold)
-  )
-}
-
-bootstrap()
+app.listen(PORT, () =>
+  console.log(`Server is running on port ${PORT}`.yellow.bold)
+)
